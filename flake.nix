a74af54aa7b8c5693789891f8d5524476eb40551
Arsen Nuramatov <x@ctrl.sn>
2025-08-07 03:01:38 +0200

    flake: specify mainProgram

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
           version = "0.1.0";
           src = ./.;
           vendorHash = "sha256-VTENHawfVpaWtr44aPjao/ZpWyYvTvyt7faMH0px35s=";
+          meta.mainProgram = "modagent";
         };
 
         apps.default = {
